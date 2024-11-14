9fb9a792493a964090f440ddfe80b3894f705e8e
Karabo-jpg <k.ojiambo@alustudent.com>
2024-11-14 02:15:56 -0800

    Task 0

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,20 +1,3 @@
--- Check if user_0d_1 exists and create if not
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
--- Check if user_0d_2 exists and create if not
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-
--- Grant root privileges to user_0d_1
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-
--- Grant specific privileges (SELECT and INSERT on user_2_db) to user_0d_2
-GRANT SELECT, INSERT ON user_2_db.* TO 'user_0d_2'@'localhost';
-
--- Apply changes
-FLUSH PRIVILEGES;
-
--- Display grants for user_0d_1
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-
--- Display grants for user_0d_2
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
-
+-- Show these users' privilages
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
