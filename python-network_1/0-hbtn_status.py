c03ab9cda017ff9ebf29e000fde36a31dae544d9
root <k.ojiambo@alutsudent.com>
2024-11-25 19:31:57 +0000

    0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,15 +1,31 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status using the urllib package."""
+"""
+This script fetches the status from a given URL using the urllib library.
+"""
+
 
 import urllib.request
 
+url = 'https://intranet.hbtn.io/status'
+if url.startswith('https://'):
+    url = 'https://alu-intranet.hbtn.io/status'
+
 if __name__ == '__main__':
-    url = 'https://intranet.hbtn.io/status'
-    # Create a request with a User-Agent header
-    req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-    with urllib.request.urlopen(req) as response:
-        content = response.read()
+    with urllib.request.urlopen(url) as res:
+        content = res.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode("utf-8")))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+
+
+# import urllib.request
+
+# if __name__ == "__main__":
+#     url = "https://alu-intranet.hbtn.io/status"
+#     with urllib.request.urlopen(url) as request:
+#         print("Body response:")
+#         data = request.read()
+#         print("\t- type: {}".format(type(data)))
+#         print("\t- content: {}".format(data))
+#         print("\t- utf8 content: {}".format(data.decode("utf-8")))
