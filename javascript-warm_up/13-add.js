486c1d486cfcb7e515dd6a1e7416850b5d98f1e9
Karabo-jpg <k.ojiambo@alustudent.com>
2025-01-21 11:01:13 +0300

    Task 13

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,13 +1,2 @@
 #!/usr/bin/node
-const myObject = {
-  type: 'object',
-  value: 12
-};
-
-// Log initial state
-console.log(`${myObject.type} + ${myObject.value}`);
-
-// Update value and log updated state
-myObject.value = 89;
-console.log(`${myObject.type} + ${myObject.value}`);
-
+exports.add = (a, b) => a + b;
