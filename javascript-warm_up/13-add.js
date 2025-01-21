8a3d8ce46a7f681646acd9108d047236beda912d
Karabo-jpg <k.ojiambo@alustudent.com>
2025-01-21 10:56:56 +0300

    Task 13

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -4,6 +4,10 @@ const myObject = {
   value: 12
 };
 
+// Log initial state
 console.log(`${myObject.type} + ${myObject.value}`);
+
+// Update value and log updated state
 myObject.value = 89;
 console.log(`${myObject.type} + ${myObject.value}`);
+
