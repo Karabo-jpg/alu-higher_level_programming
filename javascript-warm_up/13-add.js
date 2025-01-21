8277f56ac51886c6b5bb7d8fce3fd669b682b5ad
Karabo-jpg <k.ojiambo@alustudent.com>
2025-01-21 10:43:59 +0300

    Task 13

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -3,6 +3,7 @@ const myObject = {
   type: 'object',
   value: 12
 };
-console.log(myObject);
+
+console.log(`${myObject.type} + ${myObject.value}`);
 myObject.value = 89;
-console.log(myObject);
+console.log(`${myObject.type} + ${myObject.value}`);
